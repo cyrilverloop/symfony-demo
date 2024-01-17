7d7d509cf4b6bad258835011abc3034ac2636b77
Cyril VERLOOP <cyril.verloop@webqard.fr>
2024-01-17 12:50:26 +0100

    Replacing "cyril-verloop/symfony-twig-templates" by "cyril-verloop/templates-bundle".

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    CyrilVerloop\Bundle\TemplatesBundle\CyrilVerloopTemplatesBundle::class => ['all' => true],
 ];
